996143d809089a7a62182ccd97601e89bfab6af2
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-04-03 13:40:09 +0200

    logging info & Error

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,10 +1,16 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  */
 public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
     public static void main(String[] args) {
+      
         Calculator calc = new Calculator();
 
         System.out.println("Casper Zielinski");
@@ -13,5 +19,10 @@ public class Main {
         System.out.println(calc.minus(7, 21));
         System.out.println(calc.divide(7, 21));
         System.out.println(calc.multiply(21, 7));
+
+        logger.info("Das ist Info");
+        logger.error("Das ist Error");
+
+
     }
 }
